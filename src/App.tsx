fd882b30a9dd4fe662c9ad4adf298bde36f24ff8
LiKissDmd <likissdmd@gmail.com>
2024-01-10 19:33:37 +0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Order, generateMessages, getAllCoolOrders } from '@likissdmd/warframe-market-prime-trash-buyer';
+import { Button, CircularProgress, Grid, Stack, Typography } from '@mui/material';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [messages, setMessages] = useState<undefined | Array<string>>(undefined)
+  const [isLoading, setIsLoading] = useState<undefined | boolean>(undefined)
+
+  async function buttonClickerHandler() {
+    setIsLoading(true)
+
+    const allCoolOrders = await getAllCoolOrders({
+      // filterOrder: (order) => {
+      //   return order.platinum == 4 &&
+      //     order.user.status == "ingame"
+      // }
+    })
+    const messages = generateMessages(allCoolOrders)
+    setMessages(messages)
+    setIsLoading(false)
+
+  }
+  // isLoading ? (компонент загрузки) : (то что показывалось как обычно)
+  return <Grid
+    container
+    spacing={0}
+    direction="column"
+    alignItems="center"
+    justifyContent="center"
+    sx={{ minHeight: '100vh' }}
+  >
+    <Grid item xs={3}>
+      {
+        isLoading ? <CircularProgress /> : (
+          <div className="App">
+
+
+            <Button
+              onClick={buttonClickerHandler}
+            >
+              Click me
+            </Button>
+            {
+              !messages ? null : (
+                messages.length !== 0 ? (
+                  <Stack>
+                    <Typography>
+                      {messages[0]}
+                    </Typography>
+                    <Typography>
+                      {messages[1]}
+                    </Typography>
+                  </Stack>
+                ) : (
+                  <Typography>
+                    No cool orders
+                  </Typography>
+                )
+              )
+
+            }
+
+          </div>
+        )
+      }
+    </Grid>
+  </Grid>
 }
 
 export default App;
