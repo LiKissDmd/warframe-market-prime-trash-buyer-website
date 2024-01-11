a42a5e769eec8131bb8dc5e608f22f3e1ecf1e92
LiKissDmd <likissdmd@gmail.com>
2024-01-11 23:39:04 +0600

    update draft

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Order, generateMessages, getAllCoolOrders } from '@likissdmd/warframe-market-prime-trash-buyer';
-import { Button, CircularProgress, Grid, Stack, Typography } from '@mui/material';
-
+import { Box, Button, Chip, CircularProgress, Container, Grid, Stack, Typography } from '@mui/material';
+import FaceIcon from '@mui/icons-material/Face';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 
 function App() {
@@ -14,61 +15,74 @@ function App() {
     setIsLoading(true)
 
     const allCoolOrders = await getAllCoolOrders({
-      // filterOrder: (order) => {
-      //   return order.platinum == 4 &&
-      //     order.user.status == "ingame"
-      // }
+      filterOrder: (order) => {
+        return order.platinum == 4 &&
+          order.user.status == "ingame"
+      }
     })
     const messages = generateMessages(allCoolOrders)
+    
     setMessages(messages)
     setIsLoading(false)
 
   }
-  // isLoading ? (компонент загрузки) : (то что показывалось как обычно)
-  return <Grid
-    container
-    spacing={0}
-    direction="column"
-    alignItems="center"
-    justifyContent="center"
-    sx={{ minHeight: '100vh' }}
-  >
-    <Grid item xs={3}>
-      {
-        isLoading ? <CircularProgress /> : (
-          <div className="App">
+ 
 
 
-            <Button
-              onClick={buttonClickerHandler}
-            >
-              Click me
-            </Button>
-            {
-              !messages ? null : (
-                messages.length !== 0 ? (
-                  <Stack>
-                    <Typography>
-                      {messages[0]}
-                    </Typography>
-                    <Typography>
-                      {messages[1]}
-                    </Typography>
-                  </Stack>
-                ) : (
-                  <Typography>
-                    No cool orders
-                  </Typography>
-                )
-              )
+  return (
+    <Container>
+      
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: '100vh' }}
 
-            }
+      >
+        <Grid item xs={3}>
+          {
+            isLoading ? <CircularProgress /> : (
+              <div className="App">
 
-          </div>
-        )
-      }
-    </Grid>
-  </Grid>
+
+                <Button variant='outlined'
+                  onClick={buttonClickerHandler}
+                >
+                  Click me
+                </Button>
+
+                <Box marginTop={"16px"}>
+                  {
+                    !messages ? null : (
+                      messages.length !== 0 ? (
+                        <Stack spacing={1.5}>
+                          {
+                            messages.map(function (message) {
+                              return <Chip  icon={<ContentCopyIcon fontSize="small" />} label={message} variant="outlined" onClick={function () {
+                                navigator.clipboard.writeText(message)
+                              }} />
+                            })
+                          }
+                        </Stack>
+                      ) : (
+                        <Typography>
+                          No cool orders
+                        </Typography>
+                      )
+                    )
+
+                  }
+                </Box>
+
+              </div>
+            )
+          }
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 root.render(
+
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
